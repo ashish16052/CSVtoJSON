f4aafec10924462fec2c802aac25ae6b9a311fe6
Ashish Mohite <86217607+ashish16052@users.noreply.github.com>
2022-04-29 00:49:45 +0530

    add 2 separate collections

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ mongoose.connect(
     'mongodb://localhost:27017/jsonData',
     { useUnifiedTopology: true, useNewUrlParser: true }
 );
-const Schema = new mongoose.Schema({
+
+const SchemaA = new mongoose.Schema({
+    text: String,
+    email: String,
+    number: Number,
+    url: String
+})
+const SchemaB = new mongoose.Schema({
     id: Number,
     name: String,
     city: String,
     age: Number
 })
-const JsonData = new mongoose.model("JsonData", Schema)
+const CollectionA = new mongoose.model("Collection_a", SchemaA)
+const CollectionB = new mongoose.model("Collection_b", SchemaB)
 
 const multer = require('multer')
 const storage = multer.diskStorage({
@@ -34,20 +42,27 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', upload.single('csvfile'), function (req, res) {
+    const data = new CollectionA({
+        text: req.body.text,
+        email: req.body.email,
+        number: req.body.number,
+        url: 'https://my-bucket.s3-us-west-2.amazonaws.com'
+    });
+    data.save();
     res.redirect('/');
     var arr = [];
     fs.createReadStream('csvfile').pipe(csv({})).on('data', function (data) {
         arr.push(data);
     }).on('end', () => {
-        JsonData.insertMany(arr, function (err, jsonData) {
+        CollectionB.insertMany(arr, function (err, jsonData) {
             if (err) {
-              console.log(err);
+                console.log(err);
             };
             console.log(jsonData);
-          });
+        });
     });
 })
 
 app.listen(3000, () => {
-    console.log(`App listening on port 3000`)
+    console.log('App listening on port 3000')
 })
